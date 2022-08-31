55c207e0ac26cca06339b6212e37635077034d50
keipala <keipalahniuk@gmail.com>
2022-08-31 12:38:00 +0200

    Added ability to add JWT to SwaggerUI header.

--- JwtWebAPI/Program.cs
+++ JwtWebAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorize header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    // Needed to add JWT in header in SwaggerUI requests
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 // Add authentication middleware
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
